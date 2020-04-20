e003f669f4705d8418b2e18633c6a32b6e1e5bd9
lnugraha <leo.nugraha@outlook.com>
2020-04-20 15:30:41 +0800

    Modified and updated files

--- GitScripts/deleteGitBranch.sh
+++ GitScripts/deleteGitBranch.sh
@@ -5,12 +5,18 @@
 ############################################################ 
 
 function delBranch(){
-  if [[ $# -eq 0 ]]; then
+  if [[ $# -eq 0 ]]
+  then
     echo "Please specify the branch name that will be deleted";
-  else:  
+  elif [[ $# -eq 1 ]]
+  then
     git pull origin master;
     git branch -d $1;
     git push origin --delete $1;
+  else
+    echo "Branch cannot be found";
+    echo "Check all of the active branches";
+    git branch;
   fi
 }
 
---
+++ GitScripts/mastermergeGitBranch.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# Merge an existing branch with the master branch
+# git checkout master; git pull origin master;
+# git branch --merged;
+# git merge $1
+# git push origin master
+
